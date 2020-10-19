9455148f07eda2a93582e92722aaa489dacaffb7
SUN-JY <forits@qq.com>
2020-10-19 13:23:06 +0800

    one

---
+++ AndroidBaseEdu/src/main/java/com/sforits/androidbaseedu/book/example/_04/_03/Test.java
@@ -0,0 +1,15 @@
+package com.sforits.androidbaseedu.book.example._04._03;
+
+
+/**
+ * @Author：sforits
+ * @E-mail：sforits@gmail.com
+ * @Date：2020/10/15-20:25 Created by IntelliJ IDEA.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.shout();
+        System.out.println(dog.name);
+    }
+}
