b821bfe35300d69adb2440286cad0c1cf4a35d69
sforits <sforits@gmail.com>
2020-11-01 22:25:22 +0800

    java第三次作业

---
+++ AndroidBaseEdu/src/main/java/com/sforits/androidbaseedu/book/zuoye/_03/_02/Main.java
@@ -0,0 +1,13 @@
+package com.sforits.androidbaseedu.book.zuoye._03._02;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Person> people = new HashSet<>();
+        people.add(new Person("Tom", 12));
+        people.add(new Person("Jack", 12));
+        people.add(new Person("Rose", 12));
+        System.out.println(people.toString());
+    }
+}
